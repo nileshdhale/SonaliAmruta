d01d4203a0705188df0e7fd5237cad44404a94e1
nileshdhale <nilesh.rkr@gmail.com>
2022-10-01 17:41:50 +0530

    new changes

---
+++ SAprojects/src/test/java/com/tests/LoginIntoApplication.java
@@ -0,0 +1,46 @@
+package com.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LoginIntoApplication {
+	
+	//public WebDriver driver;
+	
+	@Test
+	public void Login() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Shree\\Documents\\Drivers\\chromedriver_win32\\chromedriver.exe");
+		WebDriver	driver= new ChromeDriver();
+		driver.get("https://dev2-app.simplicapital.ai/");
+		
+		driver.manage().window().maximize();
+		System.out.print("........Logged in Successfully......");
+		driver.close();
+		
+	}@Test
+	public void LogOut() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Shree\\Documents\\Drivers\\chromedriver_win32\\chromedriver.exe");
+		WebDriver	driver= new ChromeDriver();
+		driver.get("https://dev2-app.simplicapital.ai/");
+		System.out.print("........Logged Out Successfully.........");
+		
+		driver.manage().window().maximize();
+		driver.close();
+	
+	
+
+}
+	@Test
+	public void Inspection() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Shree\\Documents\\Drivers\\chromedriver_win32\\chromedriver.exe");
+		WebDriver	driver= new ChromeDriver();
+		driver.get("https://dev2-app.simplicapital.ai/");
+		System.out.print("........Inspected Successfully.........");
+		
+		driver.manage().window().maximize();
+		driver.close();
+		
+	}
+	
+}
